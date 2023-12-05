cf5ca00cba6acfcb91624113652669dc3db242ee
Raunakkushwaha <raunakkushwaha85@gmail.com>
2023-12-05 16:39:29 +0530

    final

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+import hero from './hero.jpg'
+import meta from './edu.png'
+import shopify from './edu.png'
+import starbucks from './edu.png'
+import tesla from './edu.png'
+import logo from './logo.jpg'
+
+export {
+    hero,
+    meta,
+    shopify,
+    starbucks,
+    tesla,
+    logo
+}
---
+++ src/constants/index.js
@@ -0,0 +1,227 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    }
+];
+
+export const experiences = [
+    {
+        title: "Class-X",
+        company_name: "Percetage-81%",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "March 2018 - june 2019",
+        points: [
+            "School-Harmony Internation School.",
+            "Place-Chaurasia, Ramgharh, Kaimur, (BIHAR).",
+            "From-CBSE - Central Board of Secondary Education"
+            
+        ],
+    },
+    {
+        title: "Class-XII",
+        company_name: "Percetage-72%",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "june 2020 - march 2021",
+        points: [
+            "School-Harmony Internation School.",
+            "Place-Chaurasia, Ramgharh, Kaimur, (BIHAR).",
+            "From-CBSE - Central Board of Secondary Education"
+        ],
+    },
+    {
+        title: "B.tech-CSE",
+        company_name: "Current SGPA-7.6",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "april 2021 - Ongoing",
+        points: [
+            "Collage-SR Grroup Of Institution.",
+            "Place-Ambabai, jhansi, Uttar Pradesh,(284419)",
+            "From-Dr. A.P.J. Abdul Kalam Technical University (AKTU)"
+        ],
+    },
+    // {
+    //     title: "Full stack Developer",
+    //     company_name: "Meta",
+    //     icon: meta,
+    //     iconBg: "#a2d2ff",
+    //     date: "Jan 2023 - Present",
+    //     points: [
+    //         "Developing and maintaining web applications using React.js and other related technologies.",
+    //         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+    //         "Implementing responsive design and ensuring cross-browser compatibility.",
+    //         "Participating in code reviews and providing constructive feedback to other developers.",
+    //     ],
+    // },
+];
+
+export const socialLinks = [
+    {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/Contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Raunakkushwaha/',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/raunac00/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'DJ-Notes (E-Notes Website)',
+        description: 'Notes downloading websites serve as valuable resources for accessing a diverse range of educational materials, including lecture notes, study guides, and academic resources.',
+        link: 'https://raunakkushwaha.github.io/notes-application/',
+    },
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: 'PASSWORD generator',
+        description: 'A password generator creates secure and random passwords with a mix of characters. It discourages password reuse and is often for enhanced security.',
+        link: 'https://raunakkushwaha.github.io/password-generator/',
+    },
+    {
+        iconUrl: car,
+        theme: 'btn-back-blue',
+        name: 'Car Doughe Game',
+        description: 'Car Douche is a user-friendly racing game with easy controls and straightforward gameplay. Players enjoy a casual driving experience, navigating uncomplicated tracks and power-ups.',
+        link: 'https://raunakkushwaha.github.io/car-game/',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'SpaceShip Battle Game',
+        description: ' Spaceship Battle is an accessible game with easy controls where players engage in space combat. Pilot your spaceship, dodge enemy attacks, and fire lasers in a casual.',
+        link: 'https://raunakkushwaha.github.io/space-game/',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Netflix-Clone',
+        description: ' Netflix Clone offers a user-friendly streaming platform mirroring Netflixs interface. Users can enjoy a vast library of movies and TV shows, with personalized recommendations',
+        link: 'https://raunakkushwaha.github.io/netflix-clone/',
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Coffee Restro Website',
+        description: 'Explore a rich collection of articles on coffee culture, creating a resourceful space for coffee aficionados.Enhancing your brewing experience through this dedicated online platform.',
+        link: 'https://raunakkushwaha.github.io/coffee-site/',
+    },
+    
+];
