e90b636c19a96d7ecbf0edf242a65f17ffb45391
Raunak Kushwaha <101345260+Raunakkushwaha@users.noreply.github.com>
2024-10-10 15:03:15 +0530

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -7,7 +7,7 @@ const HomeInfo = ({ currentStage }) => {
     return (
       <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
         Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Hi this is Raunak </span>
+        <span className='font-semibold mx-2 text-white'> Raunak </span>
         👋
         <br />
         Full Stack Web Developer
